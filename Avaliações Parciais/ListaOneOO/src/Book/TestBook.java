ef21484f19dd417d5c0a5572a257fb552ae12ea6
Rodolfo Dantas de Oliveira Bezerra <47856330+denpufa@users.noreply.github.com>
2019-10-01 03:11:24 -0300

    Update TestBook.java

--- Avaliações Parciais/ListaOneOO/src/Book/TestBook.java
+++ Avaliações Parciais/ListaOneOO/src/Book/TestBook.java
@@ -10,24 +10,19 @@ package Book;
 public class TestBook {
 
     public static void main(String[] args) {
-
-        // Declaração dos objetos
+    //instannciando os objetos inicializandos e os utilizando seguindo a necessidade
         Author autor1;
         Author autor2;
 
-        // Construção dos objetos
         autor1 = new Author("Nelson Rubens", "nelsonrubens@example.com", 'm');
         autor2 = new Author("Katarina Braga", "katarinabraga@example.com", 'f');
 
-        // Declaração dos objetos 
         Book book1;
         Book book2;
 
-        // Construção dos objetos
         book1 = new Book("Flores Artificiais", 20.0, autor1);
         book2 = new Book("O Capital no Século 21", 10.0, autor2);
 
-        // Utilização dos objetos
         book1.setPrice(38);
         book1.setQtyInStock(3);
         System.out.println("Titulo: " + book1.getName());
